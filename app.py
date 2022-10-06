8278884200e9b290f66b0d2febe2693cfb9cb9cf
Ramanathan AR <ARRamanathan@users.noreply.github.com>
2022-10-06 16:03:55 +0530

    Update app.py
    Dummy test

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, this is a sample Python Web App running on Flask Framework!!"
+    return "Hello, this is a sample Python Web App running on Flask Framework!! Random testing is a black-box software testing technique where programs are tested by generating random, independent inputs. Results of the output are compared against software specifications to verify that the test output is pass or fail.[1] In case of absence of specifications the exceptions of the language are used which means if an exception arises during test execution then it means there is a fault in the program, it is also used as a way to avoid biased testing."
 
 
